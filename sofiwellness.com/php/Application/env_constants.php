00a6f8963e2a8e03d1518a3939dccc453e2dc4cc
Edward Pamg <edwpang@gmail.com>
2013-03-18 00:46:00 -0400

    pointing the mysql data to remote location

--- sofiwellness.com/php/Application/env_constants.php
+++ sofiwellness.com/php/Application/env_constants.php
@@ -137,7 +137,7 @@ define('EMAIL_SENDER', 'system@greenhillhab.com');
 //------------------------------------------------
 //for database
 define ('DB_APP_TYPE', 'PDO_MYSQL');
-define ('DB_APP_HOST', 'localhost');  //or xxx.com
+define ('DB_APP_HOST', 'sofiwellness.com');  //or xxx.com
 //for source1
 define ('DB_APP_USERNAME', 'greenhi9_prog');
 define ('DB_APP_PASSWORD', '123456');
