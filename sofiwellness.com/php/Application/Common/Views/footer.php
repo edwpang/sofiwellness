bf90a6ce5930a1eca2b7aa6dbdaf4f8a7c8f8200
Edward Pamg <edwpang@gmail.com>
2013-05-21 01:11:21 -0400

    Add facebook, google plus

--- sofiwellness.com/php/Application/Common/Views/footer.php
+++ sofiwellness.com/php/Application/Common/Views/footer.php
@@ -19,7 +19,25 @@
         &nbsp;|&nbsp;
         <a href="/info/privacy" class="nodec">Privacy Statement</a>
        </p>
-		
+<!-- Google Code for website Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 990146348;
+var google_conversion_language = "en";
+var google_conversion_format = "2";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "yju3CKTSsVIQrN6R2AM";
+var google_conversion_value = 0;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/990146348/?value=0&amp;label=yju3CKTSsVIQrN6R2AM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+       
 </div> <!-- end #footer_content -->
 </div> <!-- #footer -->
 
--- sofiwellness.com/php/Application/Home/Views/HomeView.php
+++ sofiwellness.com/php/Application/Home/Views/HomeView.php
@@ -28,7 +28,11 @@ $ref_service = '/' .GlobalConstants::TABID_SERVICE;
 <blockquote>Go into partnership with nature; she does more than half the work and asks none of the fee. - <i>Martin H. Fischer</i></blockquote>
 <p>
 Sophie Wellness Centre is a <b>multi-disciplinary</b> Clinic offering a wide range of alternative and complementary therapies. Our <u>practitioners</u> will work together with you and get to the root of what gets in the way of good health. Our goal is to assist your body's natural healing ability, not only bring you back to your natural state of health, but also to prevent illness from occurring again. </p>
-
+<iframe src="https://www.facebook.com/plugins/like.php?href=https://www.facebook.com/greenhill.rehab"
+        scrolling="no" frameborder="0"
+        style="border:none; width:450px; height:80px">
+</iframe>
+        
 <table border="0" align="center" class="imgs">
 <tr>
 <td valign="top"><img src="/images/sys/entrance.jpg"></td>
